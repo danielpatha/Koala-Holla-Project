fb7c81347d1bc825c27bf7d6169b99240dbbf4e5
Mohamed Yusuf <Mohamed-yusuf2@hotmail.com>
2022-10-06 14:43:55 -0500

    Add POST

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const koalaRouter = express.Router();
+const pool = require('../modules/pool');
 
 // DB CONNECTION
 
@@ -8,7 +9,38 @@ const koalaRouter = express.Router();
 
 
 // POST
+koalaRouter.post('/', (req, res) => {
+    console.log('req.body is:', req.body);
 
+
+
+    const sqlText = `
+    INSERT INTO "koalas"
+        ("name","age","gender","ready","notes")
+    
+    VALUES
+        ($1,$2,$3,$4,$5)       
+    `;
+
+    const sqlParams = [
+        req.body.name,
+        req.body.age,
+        req.body.gender,
+        req.body.ready,
+        req.body.notes,
+    ];
+
+    console.log('in sqlText',sqlText)
+
+    pool.query(sqlText,sqlParams)
+        .then((dbRes) =>{
+            res.sendStatus(201)
+        })
+        .catch((err) =>{
+            console.log('POST not working',err)
+            res.sendStatus(500)
+        })
+    })
 
 // PUT
 
